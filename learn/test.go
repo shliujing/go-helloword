06e0baddb6afce91c81a868c852538f060cae1d5
lj <liujing@qiniu.com>
2018-11-24 18:06:29 +0800

    Initing

---
+++ learn/test.go
@@ -0,0 +1,19 @@
+package learn
+
+import (
+	"math"
+	"fmt"
+)
+
+func main(){
+
+	fmt.Println("hello go");
+
+	println(math.Abs(20))
+
+	//for i:=1; i<29 ;i++ {
+	//	if math.Pow(float64(i),float64(85))%35==6 {
+	//		println(i)
+	//	}
+	//}
+}
