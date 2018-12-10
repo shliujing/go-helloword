4ba809dab278c93e6521a931128d482cc1c118c2
lj <liujing@qiniu.com>
2018-12-10 09:49:13 +0800

    22

--- learn/basic/range.go
+++ learn/basic/range.go
@@ -4,6 +4,8 @@ import "fmt"
 
 //Go 语言中 range 关键字用于 for 循环中迭代数组(array)、切片(slice)、通道(channel)或集合(map)的元素。在数组和切片中它返回元素的索引和索引对应的值，在集合中返回 key-value 对的 key 值。不包含实际作用。
 
+
+
 func main() {
 	//这是我们使用range去求一个slice的和。使用数组跟这个很类似
 	nums := []int{2, 3, 4}
